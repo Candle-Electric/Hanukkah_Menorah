eca71f98e016bcf63f21eee599956f1001dbaad0
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-08-29 20:42:12 -0700

    Update Menorah.asm
    G.B.G.I.H.!

--- Menorah.asm
+++ Menorah.asm
@@ -187,7 +187,7 @@ Main_Loop:
 	mov #8, c
 	P_Draw_Sprite candles12_spr_address, b, c
 	mov #16, b
-	P_Draw_Sprite candles34_spr_address, b, c ; P_Draw..... "" "" ; Check If C Remains After Call To Kresna's P_Draw.
+	P_Draw_Sprite candles34_spr_address, b, c ; P_Draw..... "" "" ; Check If C Remains After Call To Kresna's P_Draw, Meaning That Candles 1&2 + Candles 3&4 Make it in the Blit.
 	mov #24, b
 	; Move C,D,R Up For Candle #5.
 	P_Draw_Sprite candle5_spr_address, b, c 
