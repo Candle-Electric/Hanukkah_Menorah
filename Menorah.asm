a68b712d4a68ff7160b1e7b83737979b640b219b
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-09-03 23:46:54 -0700

    Update Menorah.asm
    T.B.D.R.,T.C.D.R.,U.T.L.D.R.,D.D.W.B.K.,S.F.F.2.P.C.A.G.O.A.W.S.;.T.G.B.A.A.

--- Menorah.asm
+++ Menorah.asm
@@ -186,6 +186,7 @@ Main_Loop:
 	sub #3
 	bn acc, 7, .Draw_Candle_5
 	mov #<Candles34_Normal, candles34_spr_address ; Or Keep The Same?
+	mov #>Candles_Left_1, candles34_spr_address ; ... In That they'd Be Instantiated With this Already? Might Need To Do it Before Blit Each Frame, THough.
 .Draw_Candle_5
 .Draw_Graphics
 	mov #8, b
