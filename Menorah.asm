e511dec565ff49b053e710e29836660ee7dd347d
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-09-06 09:48:21 -0500

    Update Menorah.asm
    H.B.W.D.,C+L!

--- Menorah.asm
+++ Menorah.asm
@@ -189,6 +189,8 @@ Main_Loop:
 	mov #>Candles_Left_1, candles34_spr_address ; ... In That they'd Be Instantiated With this Already? Might Need To Do it Before Blit Each Frame, THough.
 .Draw_Candle_5
 	ld candles_lit
+    sub #5
+    bn acc, 7, .Draw_Candles_67
 .Draw_Graphics
 	mov #8, b
 	mov #8, c
